3a682ded5918649243bfeaeb4f6abf223d4bc68d
Damon <damonc015@gmail.com>
2022-03-27 01:11:45 -0400

    added url

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["maps.googleapis.com", "lh3.googleusercontent.com"],
-  }
+    domains: [
+      "maps.googleapis.com",
+      "lh3.googleusercontent.com",
+      "pickabite.vercel.app",
+    ],
+  },
 };
 
 module.exports = nextConfig;
